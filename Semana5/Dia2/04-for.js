a000666149ae9ca14728275a5b40afee92aea1ef
MeliCast <89621059+MeliCast@users.noreply.github.com>
2021-09-24 19:33:17 -0500

    nuevo 1

---
+++ Semana5/Dia2/04-for.js
@@ -0,0 +1,19 @@
+/**
+ * for(contador; condicional; actualización:contador){
+ *      codigo que se va a repetir
+ * }
+ */
+// i++
+// i = i + 1;
+
+for(let i = 0; i <= 10; i = i + 1){
+    console.log(`El contador es:${i}`);
+}
+
+console.log("Yo estoy al final!");
+
+let ropa = ["Bufanda","Camisa","Guantes","Jeans"]
+
+for (let i= 0; i < ropa.length; i++) {
+    console.log(ropa[i]);
+}
