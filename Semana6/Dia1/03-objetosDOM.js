aa13085df3e9c3c46805d71046184fd83eb4ffd2
MeliCast <89621059+MeliCast@users.noreply.github.com>
2021-10-01 19:10:10 -0500

    nuevo

--- Semana6/Dia1/03-objetosDOM.js
+++ Semana6/Dia1/03-objetosDOM.js
@@ -37,3 +37,56 @@ miCaja.innerHTML = "<h2>Titulo Caja</h2>";
 //background-color = backgroundColor
 miCaja.style.backgroundColor = "purple";
 miCaja.style.color = "white";
+
+let misItems = document.getElementsByClassName("lista_item");
+
+console.log(misItems);
+
+//Array.from(htmlCollection), crea un arreglo
+let arregloItems = Array.from(misItems);
+
+console.log(arregloItems);
+
+//anotación importante acerca de forEach, map, filter, find
+//forEach(function(item, indice_item){})
+arregloItems.forEach(function(item, indice, conjunto){
+    item.style.backgroundColor = "skyblue";
+    item.innerHTML = `Patata ${indice}`;
+    console.log("mostrando el arreglo", conjunto);
+});
+
+//obtener element de diferentes maneras
+//los querySelector y querySelectorAll funcionan de la misma manera que los selectores en css
+let miTexto = document.querySelector("#texto");
+
+console.log(miTexto);
+
+//querySelectorAll me da un NodelList
+let boxes = document.querySelectorAll(".box");
+
+console.log(boxes);
+
+boxes.forEach(function(bx){
+    bx.innerHTML = `<p>Soy un párrafo</p>`;
+});
+
+console.log(boxes[0].innerHTML);
+
+//crear elementos
+let series = {
+    "Squid Game",
+    "Pasión de Gavilanes",
+    "Dark",
+    "Billions",
+    "Got",
+    "Suits",
+    "Mr. Robot",
+    "House of gragons",
+    "Chernobyl",
+    "Breaking Bad",
+};
+
+//createElement("etiqueta") :"p", "ul", "h1", "td"
+let lista = document.createElement("ul");
+
+divContenido.appendChild(lista);
